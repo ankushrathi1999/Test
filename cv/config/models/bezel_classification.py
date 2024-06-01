953c62f9b902c30d0c9f5915b596f15ae82722ae
rbeternal <rohit@eternalrobotics.com>
2024-06-01 08:02:28 +0530

    updated bezel classification model config with yxa and other missing bezels

--- cv/config/models/bezel_classification.py
+++ cv/config/models/bezel_classification.py
@@ -7,6 +7,8 @@ class BezelClassificationModel:
     target_detections = {BezelGroupDetectionModel.CLASS_CONTAINER}
 
     ordered_class_list = [
+        '73826M66T00-5PK',
+        '73826M66T10-5PK',
         '73831M79M00-V6N',
         '73831M79M10-V6N',
         '73831M79M20-V6N',
@@ -14,9 +16,17 @@ class BezelClassificationModel:
         '73833M73R00-V6N',
         '73833M73R10-5PK',
         '73833M73R10-V6N',
+        '73833M73R50-5PK',
     ]
+    # Missing bezels
+    # 73831M79M00-5PK BLACK W/O HOLE, BEZEL YL1 (RHD)
+    # 73831M79M10-5PK BLACK W/O HOLE, BEZEL YL1 (LHD)
+    # 73831M79M20-5PK BLACK W/ HOLE, BEZEL YL1 (RHD)
+    # 73826M66T20-5PK '11 HOLES, BEZEL YXA',
 
     class_names = {
+        '73826M66T00-5PK': '7 HOLES, BEZEL YXA',
+        '73826M66T10-5PK': '10 HOLES, BEZEL YXA',
         '73831M79M00-V6N': 'BEIGE W/O HOLE, BEZEL YL1',
         '73831M79M10-V6N': 'BEIGE W/O HOLE, BEZEL YL1',
         '73831M79M20-V6N': 'BEIGE W/ HOLE, BEZEL YL1',
@@ -24,9 +34,12 @@ class BezelClassificationModel:
         '73833M73R00-V6N': 'BEIGE W/O HOLE, BEZEL YHB',
         '73833M73R10-5PK': 'BLACK W/ HOLE, BEZEL YHBC',
         '73833M73R10-V6N': 'BEIGE W/ HOLE, BEZEL YHB',
+        '73833M73R50-5PK': 'BLACK W/O HOLE, BEZEL YHB',
     }
 
     class_colors = {
+        '73826M66T00-5PK': color_green,
+        '73826M66T10-5PK': color_green,
         '73831M79M00-V6N': color_green,
         '73831M79M10-V6N': color_green,
         '73831M79M20-V6N': color_green,
@@ -34,4 +47,5 @@ class BezelClassificationModel:
         '73833M73R00-V6N': color_green,
         '73833M73R10-5PK': color_green,
         '73833M73R10-V6N': color_green,
+        '73833M73R50-5PK': color_green,
     }
