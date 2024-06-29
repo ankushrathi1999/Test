5f37f0f15cf0b8f0d275158f03ad8a672f01f124
rbeternal <rohit@eternalrobotics.com>
2024-06-29 11:55:14 +0530

    shift time fixed to 3.15pm

--- cv/utils/shift_utils.py
+++ cv/utils/shift_utils.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 def get_current_shift():
     now = datetime.now()
-    shift_end = now.replace(hour=15, minute=30, second=0, microsecond=0) # 3.30 pm
+    shift_end = now.replace(hour=15, minute=15, second=0, microsecond=0) # 3.15 pm
     return "shiftA" if now < shift_end else "shiftB"
