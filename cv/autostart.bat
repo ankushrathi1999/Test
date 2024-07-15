50b348cf8799a82ecfc96b81ea03409992e8b659
rbeternal <rohit@eternalrobotics.com>
2024-07-15 16:53:08 +0530

    autostart and logging update

---
+++ cv/autostart.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Administrator\Documents\MSIL-IPCluster-pipeline_Dep\MSIL-IPCluster_v2\MSIL-IPCluster\cv
+C:\Users\Administrator\Documents\ipcluster_setup\engine\Scripts\python.exe main.py
