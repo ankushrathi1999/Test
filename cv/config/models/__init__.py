c15d767510387ec7929223e998f136bb20a44d52
rbeternal <137377053+rbeternal@users.noreply.github.com>
2024-12-02 16:52:38 +0530

    Update __init__.py

--- cv/config/models/__init__.py
+++ cv/config/models/__init__.py
@@ -2,6 +2,11 @@ from .part_detection_in_bottom import PartDetectionInBottomModel
 from .part_detection_in_top import PartDetectionInTopModel
 from .part_detection_out import PartDetectionOutModel
 
+from .cap_bzl_classification import CapBzlClassificationModel
+from .grns_fr_inr_classification import GrnsFrInrClassificationModel
+from .hdl_sid_in_classification import HdlSidInClassificationModel
+from .mirror_classification import MirrorClassificationModel
+
 detection_models = [
     PartDetectionInBottomModel,
     PartDetectionInTopModel,
@@ -9,5 +14,8 @@ detection_models = [
 ]
 
 classification_models = [
-
+    CapBzlClassificationModel,
+    GrnsFrInrClassificationModel,
+    HdlSidInClassificationModel,
+    MirrorClassificationModel,
 ]
