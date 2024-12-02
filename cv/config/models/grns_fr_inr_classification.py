9b673533f627617c5f954dfbdf3f8d479d79839e
rbeternal <137377053+rbeternal@users.noreply.github.com>
2024-12-02 16:42:58 +0530

    Create grns_fr_inr_classification.py

---
+++ cv/config/models/grns_fr_inr_classification.py
@@ -0,0 +1,17 @@
+from .part_detection_in_top import PartDetectionInTopModel
+from ..colors import color_green, color_red
+
+class GrnsFrInrClassificationModel:
+    name = 'grns_fr_inr'
+    imgsz = 128
+    target_detections = {PartDetectionInTopModel.CLASS_GRNS_FR_INR}
+
+    ordered_class_list = [
+        '84750M66T00-5PK',
+        '84750M72R00-5PK',
+        '84750M72R10-5PK',
+        '84750M79M00-5PK',
+    ]
+
+    class_names = {k: 'GRNS_FR_INR' for k in ordered_class_list}
+    class_colors = {k: color_green for k in ordered_class_list}
